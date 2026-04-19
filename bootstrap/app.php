2ef03ccf64b17770b264a43efcecf469fa2adace
ahmadrabbani <ahmad.fcc@gmail.com>
2026-04-20 00:40:06 +0500

    Recover cleanly from invalid Auth0 session state

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,8 +1,10 @@
 <?php
 
+use Auth0\SDK\Exception\InvalidTokenException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -14,5 +16,14 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->render(function (InvalidTokenException $exception, Request $request) {
+            if ($request->hasSession()) {
+                $request->session()->invalidate();
+                $request->session()->regenerateToken();
+            }
+
+            return redirect()
+                ->to(route('home'))
+                ->with('status', 'Your session expired or became invalid. Please sign in again.');
+        });
     })->create();
